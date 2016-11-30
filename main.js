c10c1e03c2b0fe7cb5b76f37f87fb4535cea8a77
s.murakami <smurakami.84@gmail.com>
2016-11-30 16:24:35 +0900

    完成

--- main.js
+++ main.js
@@ -1,34 +1,35 @@
 var http = require("http");
 var server = http.createServer(function(req,res) {
-    res.write("Hello World!!");
-    res.end();
+res.write("Hello World!!");
+res.end();
 });
 
 // socket.ioの準備
 var io = require('socket.io')(server);
+clients = {};
 
 // クライアント接続時の処理
 io.on('connection', function(socket) {
-    console.log("client connected!!")
+	console.log("client connected!!")
+	clients[socket.id] = socket;
 
-    // クライアント切断時の処理
-    socket.on('disconnect', function() {
-        console.log("client disconnected!!")
-    });
-    // クライアントからの受信を受ける (socket.on)
-    socket.on("from_client", function(obj){
-    		var clients = io.sockets.clients();
-        console.log(obj)
-    });
+	// クライアント切断時の処理
+	socket.on('disconnect', function() {
+		delete clients[socket.id]
+	});
+	// クライアントからの受信を受ける (socket.on)
+	socket.on("from_client", function(obj){
+		var keys = Object.keys(clients);
+		if (keys.length > 1) {
+			keys.some(function(v, i){
+				if (v == socket.id) {
+					keys.splice(i,1);    
+				}
+			});
+		}
+		var key = keys[Math.floor(keys.length * Math.random())]
+		clients[key].emit("from_server", {id: key});
+	});
 });
 
-// // とりあえず一定間隔でサーバ時刻を"全"クライアントに送る (io.emit)
-// var send_servertime = function() {
-//     var now = new Date();
-//     io.emit("from_server", now.toLocaleString());
-//     console.log(now.toLocaleString());
-//     setTimeout(send_servertime, 1000)
-// };
-// send_servertime();
-
-server.listen(8080);
+server.listen(61130);
